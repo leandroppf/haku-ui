115665dc28b00feb968c7439b5dbd3df25bc620e
Leandro Pedroso <lpedroso@meudroz.com>
2022-10-16 00:05:39 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="max-w-sm bg-haku-cyan-500">
+      <h1 className="text-haku-xs">Hello World</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    colors: {
+      "haku-cyan": {
+        500: "red",
+      },
+    },
+    fontSize: {
+      "haku-xs": "0.85rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
